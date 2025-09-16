0f5935e5b5437be0754040a4106396ba545b1220
Diego Cardozo <dcardozo18@gmail.com>
2025-09-16 01:53:21 +0000

    utlizar las imagenes flores.png y hexagono.png que estan en src/app como

--- src/components/wedding/Hero.tsx
+++ src/components/wedding/Hero.tsx
@@ -1,13 +1,33 @@
 import Image from 'next/image';
 import Countdown from './Countdown';
-import { PlaceHolderImages } from '@/lib/placeholder-images';
+import flores from '@/app/flowers.png';
+import hexagono from '@/app/hexagono.png';
+
 
 export default function Hero() {
-  const heroImage = PlaceHolderImages.find(p => p.id === 'wedding-hero');
 
   return (
     <div className="relative text-[#634F34] w-full h-[80vh] min-h-[600px] flex items-center justify-center text-center overflow-hidden">
-      <div className="absolute inset-0" />
+        <div className="absolute inset-0 z-0">
+            <Image
+            src={hexagono}
+            alt="hexagon background"
+            layout="fill"
+            objectFit="cover"
+            quality={100}
+            priority
+            />
+        </div>
+        <div className="absolute inset-0 z-0">
+            <Image
+            src={flores}
+            alt="flowers background"
+            layout="fill"
+            objectFit="cover"
+            quality={100}
+            priority
+            />
+      </div>
       <div className="relative z-10 p-4 animate-fade-in-up">
         <h1 className="text-6xl md:text-8xl font-headline" style={{ textShadow: '2px 2px 8px rgba(0,0,0,0.5)', lineHeight: "80px", fontSize: "8rem", fontFamily: "'Mea Culpa', cursive" }}>
           Ricardo<br /><span className="text-primary" style={{ fontSize: "6rem", fontFamily: "'Mea Culpa', cursive" }}>&</span><br />Rocio
